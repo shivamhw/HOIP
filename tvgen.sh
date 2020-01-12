71cff6c272205c918416891d9dd30b5c3f80358e
shivamhw <s.kmishra703@gmail.com>
2020-01-12 20:09:37 +0530

    first

---
+++ tvgen.sh
@@ -0,0 +1,64 @@
+wget -q -d --header="authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxODA4MjQwNTE1NjQzODE0NDIiLCJ0b2tlbiI6IkpPU28tZnlRbS1wbzdsLXFFVU8tNlZaUS1FMlZULU5aIiwiZXhwaXJhdGlvbk1vbWVudCI6IjIwMjEtMDItMDRUMDk6NTc6MDQuOTM1WiIsImlzUHJvZmlsZUNvbXBsZXRlIjoidHJ1ZSIsImNoYW5uZWxQYXJ0bmVySUQiOiJNU01JTkQiLCJmaXJzdE5hbWUiOiJZb3V0dWJlIiwibGFzdE5hbWUiOiJTaGFoIiwic2Vzc2lvbkNyZWF0aW9uVGltZSI6IjIwMTktMTItMTVUMTg6NTc6MDQuOTk4WiIsIm1vYmlsZU51bWJlciI6Ijk0Nzk5NDA5NjYiLCJkYXRlT2ZCaXJ0aCI6IjQxNDg3MzAwMDAwMCIsImdlbmRlciI6Ik1hbGUiLCJwcm9maWxlUGljIjoiIiwic29jaWFsUHJvZmlsZVBpYyI6Imh0dHBzOi8vY2RuLm9uZS5hY2NlZG8udHYvZmlsZXMvNWQzZWJiYzIyM2VlYzYwMDEwODk5NmY3P3Nlc3Npb25LZXk9MFNTS0VCMjI1MkFGQ0RBQzI4RUYzQzcxRkIxM0ZEMjk1NDM5I2Fzc2V0Iiwic29jaWFsTG9naW5JRCI6IjExMzk2NDI3OTU4MDU4NDA4ODAwNiIsInNvY2lhbExvZ2luVHlwZSI6Ikdvb2dsZSIsImlzRW1haWxWZXJpZmllZCI6InRydWUiLCJpc01vYmlsZVZlcmlmaWVkIjoidHJ1ZSIsImVtYWlsIjoiYWxva2pzaGFoQGdtYWlsLmNvbSIsImlhdCI6MTU3NjQzNjIyNSwiZXhwIjoxNjEyNDMyNTY1fQ.ee5TPZgJnw_gnnP0xZj9BcvEyVGgEAQq_Vtcfjqm2e0" -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36" -O - 'https://www.sonyliv.com/api/v5/vod/asset/playbackUrl?assetId=4867235330001&browser=Chrome&os=others&platform=web' > sonyt
+
+
+wget -q -d --header="authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxODA4MjQwNTE1NjQzODE0NDIiLCJ0b2tlbiI6IkpPU28tZnlRbS1wbzdsLXFFVU8tNlZaUS1FMlZULU5aIiwiZXhwaXJhdGlvbk1vbWVudCI6IjIwMjEtMDItMDRUMDk6NTc6MDQuOTM1WiIsImlzUHJvZmlsZUNvbXBsZXRlIjoidHJ1ZSIsImNoYW5uZWxQYXJ0bmVySUQiOiJNU01JTkQiLCJmaXJzdE5hbWUiOiJZb3V0dWJlIiwibGFzdE5hbWUiOiJTaGFoIiwic2Vzc2lvbkNyZWF0aW9uVGltZSI6IjIwMTktMTItMTVUMTg6NTc6MDQuOTk4WiIsIm1vYmlsZU51bWJlciI6Ijk0Nzk5NDA5NjYiLCJkYXRlT2ZCaXJ0aCI6IjQxNDg3MzAwMDAwMCIsImdlbmRlciI6Ik1hbGUiLCJwcm9maWxlUGljIjoiIiwic29jaWFsUHJvZmlsZVBpYyI6Imh0dHBzOi8vY2RuLm9uZS5hY2NlZG8udHYvZmlsZXMvNWQzZWJiYzIyM2VlYzYwMDEwODk5NmY3P3Nlc3Npb25LZXk9MFNTS0VCMjI1MkFGQ0RBQzI4RUYzQzcxRkIxM0ZEMjk1NDM5I2Fzc2V0Iiwic29jaWFsTG9naW5JRCI6IjExMzk2NDI3OTU4MDU4NDA4ODAwNiIsInNvY2lhbExvZ2luVHlwZSI6Ikdvb2dsZSIsImlzRW1haWxWZXJpZmllZCI6InRydWUiLCJpc01vYmlsZVZlcmlmaWVkIjoidHJ1ZSIsImVtYWlsIjoiYWxva2pzaGFoQGdtYWlsLmNvbSIsImlhdCI6MTU3NjQzNjIyNSwiZXhwIjoxNjEyNDMyNTY1fQ.ee5TPZgJnw_gnnP0xZj9BcvEyVGgEAQq_Vtcfjqm2e0" -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36" -O - 'https://www.sonyliv.com/api/v5/vod/asset/playbackUrl?assetId=5492474426001&browser=Chrome&os=others&platform=web' > maxt
+wget -q -d --header="authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxODA4MjQwNTE1NjQzODE0NDIiLCJ0b2tlbiI6IkpPU28tZnlRbS1wbzdsLXFFVU8tNlZaUS1FMlZULU5aIiwiZXhwaXJhdGlvbk1vbWVudCI6IjIwMjEtMDItMDRUMDk6NTc6MDQuOTM1WiIsImlzUHJvZmlsZUNvbXBsZXRlIjoidHJ1ZSIsImNoYW5uZWxQYXJ0bmVySUQiOiJNU01JTkQiLCJmaXJzdE5hbWUiOiJZb3V0dWJlIiwibGFzdE5hbWUiOiJTaGFoIiwic2Vzc2lvbkNyZWF0aW9uVGltZSI6IjIwMTktMTItMTVUMTg6NTc6MDQuOTk4WiIsIm1vYmlsZU51bWJlciI6Ijk0Nzk5NDA5NjYiLCJkYXRlT2ZCaXJ0aCI6IjQxNDg3MzAwMDAwMCIsImdlbmRlciI6Ik1hbGUiLCJwcm9maWxlUGljIjoiIiwic29jaWFsUHJvZmlsZVBpYyI6Imh0dHBzOi8vY2RuLm9uZS5hY2NlZG8udHYvZmlsZXMvNWQzZWJiYzIyM2VlYzYwMDEwODk5NmY3P3Nlc3Npb25LZXk9MFNTS0VCMjI1MkFGQ0RBQzI4RUYzQzcxRkIxM0ZEMjk1NDM5I2Fzc2V0Iiwic29jaWFsTG9naW5JRCI6IjExMzk2NDI3OTU4MDU4NDA4ODAwNiIsInNvY2lhbExvZ2luVHlwZSI6Ikdvb2dsZSIsImlzRW1haWxWZXJpZmllZCI6InRydWUiLCJpc01vYmlsZVZlcmlmaWVkIjoidHJ1ZSIsImVtYWlsIjoiYWxva2pzaGFoQGdtYWlsLmNvbSIsImlhdCI6MTU3NjQzNjIyNSwiZXhwIjoxNjEyNDMyNTY1fQ.ee5TPZgJnw_gnnP0xZj9BcvEyVGgEAQq_Vtcfjqm2e0" -U "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36" -O - 'https://www.sonyliv.com/api/v5/vod/asset/playbackUrl?assetId=4867316278001&browser=Chrome&os=others&platform=web' > sabt
+
+bash zeetok
+
+zee="https://z5ams.akamaized.net/zeetvhd/index.m3u8"
+andtv="https://z5ams.akamaized.net/andtvhd/index.m3u8"
+andpic="https://z5ams.akamaized.net/andpicssd/index.m3u"
+
+zeetoken=`cat zeet | jq -r ".video_token"`
+eua=""
+#eua="ExoPlayerDemo/2.0 (Linux;Android 5.0.1) ExoPlayerLib/2.7.3/19.0"
+#eua="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+
+
+echo "#EXTINF:1, SonyHD" > try.m3u8
+echo `cat sonyt | jq -r ".playbackUrl"`"|User-Agent=$eua" >> try.m3u8
+
+echo "#EXTINF:2, SABHD" >> try.m3u8
+echo `cat sabt | jq -r ".playbackUrl"`"|User-Agent=$eua" >> try.m3u8
+
+
+echo "#EXTINF:12, MaxSD" >> try.m3u8
+echo `cat maxt | jq -r ".playbackUrl"`"|User-Agent=$eua" >> try.m3u8
+
+echo "#EXTINF:3, ZeeHD" >> try.m3u8
+echo "$zee$zeetoken|User-Agent=$eua" >> try.m3u8
+
+echo "#EXTINF:4, AndTVHD" >> try.m3u8
+echo "$andtv$zeetoken|User-Agent=$eua" >> try.m3u8
+
+echo "#EXTINF:5, AndPictures" >> try.m3u8
+echo "$andpic$zeetoken|User-Agent=$eua" >> try.m3u8
+
+echo "#EXTINF:6, StarPlus" >> try.m3u8
+echo "http://146.88.24.48/hotstar/hsplus.php?c=starplushd&e=.m3u8" >> try.m3u8
+
+echo "#EXTINF:8, ColrsSD" >> try.m3u8
+echo 'http://ms-azure-video.gslb.startimestv.com/live/COLORS_1072_500_480x360_424212/playlist.m3u8|User-Agent=ExoPlayerDemo/2.0 (Linux;Android 5.0.1) ExoPlayerLib/2.7.3/19.0' >> try.m3u8
+
+echo "#EXTINF:9, StarHD" >> try.m3u8
+echo "http://starplushd-i.akamaihd.net/hls/live/569928-b/starplushd/master_1200.m3u8|User-Agent=Hotstar;in.startv.hotstar/8.1.6 (Linux;Android 5.0.1) ExoPlayerLib/2.9.5" >> try.m3u8 
+
+echo "#EXTINF:10, StarMOVIESHD" >> try.m3u8
+echo "http://livecdnh1.tvanywhere.ae/hls/star_movies/index.m3u8" >> try.m3u8
+
+echo "#EXTINF:11, BB" >> try.m3u8 
+echo "https://vootlivelinear.akamaized.net/hls/live/2008467/VootBBH13/master.m3u8" >> try.m3u8
+
+
+echo "#EXTINF:12, Aaj Tak HD" >> try.m3u8 
+echo "https://feeds.intoday.in/aajtak/api/aajtakhd/master.m3u8" >> try.m3u8
+
+echo "#EXTINF:13, NDTV India" >> try.m3u8 
+echo "https://ndtvindiaelemarchana.akamaized.net/hls/live/2003679/ndtvindia/ndtvindiamaster.m3u8" >> try.m3u8
+
+
+
+echo "#EXTINF:19, StarGOLDHD" >> try.m3u8
+echo "https://staragold-i.akamaihd.net/hls/live/569910-b/staragold/master_1200.m3u8|User-Agent=Hotstar;in.startv.hotstar/8.1.6 (Linux;Android 5.0.1) ExoPlayerLib/2.9.5" >> try.m3u8 
+
